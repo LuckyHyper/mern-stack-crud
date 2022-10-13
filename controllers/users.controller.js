5fe1f6cf9f66427c5e4b9bd0dee075f78e652ebf
joharyfluks <hechmi.benhadjali@gmail.com>
2022-10-13 16:18:03 +0100

    crud APIs done

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,21 +1,55 @@
-const AddUser = async (req,res) => {
-    res.send('add test')
+const { findOne, findOneAndRemove } = require('../models/users.models');
+const Users = require('../models/users.models');
+
+
+const AddUser =  async(req,res) => {
+    try {
+        await Users.create(req.body);
+        console.log(req.body);
+        res.status(200).json({message: 'Users added successfully..'});    
+    } catch (error) {
+       console.log(error.message);
+    }
 }
 
 const FindAllUsers = async (req,res) => {
-    res.send('find all test')
+    try{
+        const data = await Users.find();
+        res.status(200).send(data);
+    } catch(error) {
+        res.status(404).send({message : "Could not find users!!"})
+    }
 }
 
 const FindSingleUser = async (req,res) => {
-    res.send('find one test')
+    try{
+        const data = await Users.findOne({_id:req.params.id});
+        res.status(200).send(data);
+    } catch(error) {
+        res.status(404).send({message : "Could not find this user!!"})
+    }
 }
 
 const UpdateUser = async (req,res) => {
-    res.send('update test')
+    try {
+        const data = await Users.findOneAndUpdate(
+            {_id:req.params.id},
+            req.body,
+            {new : true}
+            );
+            res.status(201).json(data)
+    } catch(error) {
+        res.status(500).send({error: error})
+    }
 }
 
 const DeleteUser = async (req,res) => {
-    res.send('delete test')
+    try {
+        const data = await Users.deleteOne({_id: req.params.id});
+        res.status(200).send("Delete Successfully \n");
+    } catch (error) {
+        res.send({error: error});
+    }
 }
 
 module.exports = {
